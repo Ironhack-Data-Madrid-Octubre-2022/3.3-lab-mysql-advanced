e22287c943bd19767eacfb5843883f65fb9ac1dd
aliciahernansanz <aliciahb2@gmail.com>
2022-11-02 15:44:45 +0100

    challenge 2 3

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -28,3 +28,52 @@ on ta.title_id=s.title_id
 group by ta.au_id
 order by total_profit desc limit 3;
 
+
+#CHALLENGE 2: STEP 1
+
+create temporary table step1
+select t.title_id,ta.au_id,(t.price * s.qty * t.royalty / 100 * ta.royaltyper/100) as total_royalty
+from titles as t
+left join titleauthor as ta
+on t.title_id=ta.title_id
+left join sales as s
+on ta.title_id=s.title_id;
+
+#CHALLENGE 2: STEP 2
+
+create temporary table step2
+select ta.au_id,t.title_id,sum(t.price * s.qty * t.royalty / 100 * ta.royaltyper/100) as total_royalty
+from titleauthor as ta
+left join titles as t
+on ta.title_id=t.title_id
+left join sales as s
+on ta.title_id=s.title_id
+group by ta.au_id, t.title_id;
+
+#CHALLENGE 2: STEP 3
+
+create temporary table step3
+select ta.au_id,sum((t.price * s.qty * t.royalty / 100 * ta.royaltyper/100)+t.advance) as total_profit
+from titleauthor as ta
+left join titles as t
+on ta.title_id=t.title_id
+left join sales as s
+on ta.title_id=s.title_id
+group by ta.au_id
+order by total_profit desc limit 3;
+
+
+#CHALLENGE 3: STEP 1
+
+create table most_profiting_authors
+select ta.au_id,sum((t.price * s.qty * t.royalty / 100 * ta.royaltyper/100)+t.advance) as total_profit
+from titleauthor as ta
+left join titles as t
+on ta.title_id=t.title_id
+left join sales as s
+on ta.title_id=s.title_id
+group by ta.au_id
+order by total_profit desc limit 3;
+
+
+
