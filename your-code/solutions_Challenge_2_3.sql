e521c2ef62b28958136e1c17a539f34072e82622
Iñigo Larrea <pombo7676@gmail.com>
2022-11-03 10:24:36 +0100

    Lab terminado

---
+++ your-code/solutions_Challenge_2_3.sql
@@ -0,0 +1,19 @@
+
+create table most_profiting_authors
+SELECT AUTHOR_ID, LAST_NAME, FIRST_NAME, sum(z.Profit_per_book) as total_profit
+from(SELECT ta.au_id as AUTHOR_ID, au.au_lname as LAST_NAME, au.au_fname as FIRST_NAME, ta.title_id,t.price, t.advance,  t.royalty, sum(s.qty),ta.royaltyper, (t.advance+(t.price*t.royalty*0.01*ta.royaltyper*0.01)*sum(s.qty)) as Profit_per_book
+from authors as au
+left join titleauthor as ta
+on au.au_id = ta.au_id
+left join titles as t
+on ta.title_id = t.title_id
+left join sales as s
+on t.title_id = s.title_id
+GROUP BY AUTHOR_ID, ta.title_id) as z
+
+GROUP BY AUTHOR_ID
+order by total_profit DESC
+limit 3;
+
+
+
